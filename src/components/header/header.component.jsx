1d6c87d3b3f68e5e98386a0cb221f02b5046c6c3
Michael David Galibut <davekoder@gmail.com>
2020-11-05 15:36:49 +0800

    updated dependencies

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,38 +11,35 @@ import { ReactComponent as Logo } from "../../assets/compass.svg";
 import "./header.styles.scss";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        Shop
+    <div className="options">
+      <Link className="option" to="/shop">
+        Shopping
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/shop">
         Contact
       </Link>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <div className="option" onClick={() => auth.signOut()}>
           Sign Out
         </div>
       ) : (
-        <Link className='option' to='/signin'>
+        <Link className="option" to="/signin">
           Sign In
         </Link>
       )}
       <CartIcon />
     </div>
-      {
-        hidden ? null :
-        <CartDropdown />
-      }
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
-  hidden
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
